6bb64813780827f885c0e94125fd1d67f73acd35
vipforgit <vip.ticket@mail.ru>
2020-09-23 20:12:22 +0300

    create: added slide pic with keyboard arrows

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
 
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
