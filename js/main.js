96d2cc9d72897204da4be593f638df5ccfa8984f
vipforgit <vip.ticket@mail.ru>
2020-10-02 19:56:25 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -33,4 +33,10 @@ var reviewsSlider = new Swiper(".reviews-slider", {
 
 $(".newsletter").parallax({
     imageSrc: "./img/newsletter-bgjpg.jpg"
-})
+});
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке меню");
+    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+});
